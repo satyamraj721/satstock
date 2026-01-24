c86c0520411ad01ca1e314975f71840fda09e392
SATYAM RAJ <contactrajsatyam@gmail.com>
2026-01-24 14:52:36 +0530

    Refactor HoldingsModel to use mongoose.model

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,7 +1,7 @@
-const { model } = require("mongoose");
+const mongoose = require("mongoose");
 
 const { HoldingsSchema } = require("../schemas/HoldingsSchema");
 
-const HoldingsModel = new model("holding", HoldingsSchema);
+const HoldingsModel = mongoose.model("Holding", HoldingsSchema);
 
-module.exports = { HoldingsModel };
+module.exports = HoldingsModel;
