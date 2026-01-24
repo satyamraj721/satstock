12425db1a7ee25743f63d3d0339959b0d943fda6
SATYAM RAJ <contactrajsatyam@gmail.com>
2026-01-24 14:54:44 +0530

    Refactor UserModel.js for quote style and password method
    Updated string quotes from single to double in UserModel.js. Refactored password comparison method to remove async/await.

--- backend/model/UserModel.js
+++ backend/model/UserModel.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -26,8 +26,9 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -37,11 +38,11 @@ userSchema.pre('save', async function (next) {
   }
 });
 
-// Method to compare password
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+// Compare password
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
-const UserModel = mongoose.model('User', userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
-module.exports = { UserModel };
+module.exports = UserModel;
