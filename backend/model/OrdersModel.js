69d05b20afd1ae10bb2b7cb06c8f554579aecce6
SATYAM RAJ <contactrajsatyam@gmail.com>
2026-01-24 14:51:08 +0530

    Refactor OrdersModel to use mongoose directly

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -1,7 +1,7 @@
-const { model } = require("mongoose");
+const mongoose = require("mongoose");
 
 const { OrdersSchema } = require("../schemas/OrdersSchema");
 
-const OrdersModel = new model("order", OrdersSchema);
+const OrdersModel = mongoose.model("Order", OrdersSchema);
 
-module.exports = { OrdersModel };
+module.exports = OrdersModel;
