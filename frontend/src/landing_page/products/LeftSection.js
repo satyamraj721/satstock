d582166a3ddde1de3e41737f8fb3357655670a51
SATYAM RAJ <contactrajsatyam@gmail.com>
2026-02-02 17:05:15 +0530

    Refactor product links and store badges in LeftSection

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -11,52 +11,49 @@ function LeftSection({
 }) {
   return (
     <div className="container mt-5">
-      <div className="row">
+      <div className="row align-items-center">
         <div className="col-6 p-3">
           <img src={imageURL} alt={productName} />
         </div>
-        <div className="col-6 p-5 mt-5">
+
+        <div className="col-6 p-5">
           <h1>{productName}</h1>
           <p>{productDesription}</p>
-          <div>
-            {tryDemo ? (
-              <a href={tryDemo}>Try Demo</a>
-            ) : (
-              <button type="button">Try Demo</button>
-            )}
-            {learnMore ? (
-              <a href={learnMore} style={{ marginLeft: "50px" }}>
-                Learn More
-              </a>
-            ) : (
-              <button type="button" style={{ marginLeft: "50px" }}>
-                Learn More
-              </button>
-            )}
+
+          {/* Links */}
+          <div className="product-links">
+            <a
+              href={tryDemo || "#"}
+              className="product-link"
+            >
+              Try Demo
+            </a>
+
+            <a
+              href={learnMore || "#"}
+              className="product-link"
+              style={{ marginLeft: "50px" }}
+            >
+              Learn More
+            </a>
           </div>
+
+          {/* Store badges */}
           <div className="mt-3">
-            {googlePlay ? (
-              <a href={googlePlay}>
-                <img src="media/images/googlePlayBadge.svg" alt="Google Play badge" />
-              </a>
-            ) : (
-              <img src="media/images/googlePlayBadge.svg" alt="Google Play badge" />
-            )}
-            {appStore ? (
-              <a href={appStore}>
-                <img
-                  src="media/images/appstoreBadge.svg"
-                  style={{ marginLeft: "50px" }}
-                  alt="App Store badge"
-                />
-              </a>
-            ) : (
+            <a href={googlePlay || "#"}>
+              <img
+                src="media/images/googlePlayBadge.svg"
+                alt="Google Play badge"
+              />
+            </a>
+
+            <a href={appStore || "#"}>
               <img
                 src="media/images/appstoreBadge.svg"
                 style={{ marginLeft: "50px" }}
                 alt="App Store badge"
               />
-            )}
+            </a>
           </div>
         </div>
       </div>
