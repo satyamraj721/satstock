7d6f1b3de0df850a3f095c7835701cbae0e3a26c
SATYAM RAJ <contactrajsatyam@gmail.com>
2026-02-02 16:51:35 +0530

    Import support.css in SupportPage component
    Added CSS import for styling the support page.

--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
+import "./support.css";   // 👈 IMPORTANT
 
 function SupportPage() {
   return (
-    <>
+    <div className="support-page">
       <Hero />
       <CreateTicket />
-    </>
+    </div>
   );
 }
 
